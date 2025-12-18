cb6adcd0889e753bae71134e28d97c7fa0ab493c
Varunkumar23 <sarabuvarunkumar@gmail.com>
2025-12-18 16:27:57 +0530

    Functionalities of a local variable and its rules

---
+++ Variables.java
@@ -0,0 +1,9 @@
+class Variables 
+{
+	public static void main(String[] args) 
+	{
+		int a=9;
+		int a=10;
+		System.out.println(a);
+	}
+}
---
+++ primitiveDataTypes.java
@@ -0,0 +1,45 @@
+class   
+{
+	public static void main(String[] args) 
+	{
+		byte b;
+		b=1;
+		System.out.println(b);
+		
+		short s;
+		s=2;
+		System.out.println(s);
+		
+		int i;
+		i=10;
+		System.out.println(i);
+								
+		long l;
+		l=789876;
+		System.out.println(l);
+		
+		float f;
+		f=3.5f;
+		System.out.println(f);
+		
+		double d;
+		d=12.1234;
+		System.out.println(d);
+		
+		char c;
+		c='a';
+		System.out.println(c);
+		
+		boolean status;
+		status=true;
+		System.out.println(status);
+		
+
+
+
+
+
+
+
+	}
+}
