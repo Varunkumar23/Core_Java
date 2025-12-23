8db122c6903544c11de317b0655d83e0a602ec3d
Varunkumar23 <sarabuvarunkumar@gmail.com>
2025-12-23 16:45:53 +0530

    Completed with the methods introduction

--- src/DecisionMaking/AdvancePatterns.java
+++ src/DecisionMaking/AdvancePatterns.java
@@ -1,8 +1,8 @@
 package DecisionMaking;
 
 public class AdvancePatterns {
-public static void main(String[] args) {
-	int n=4;
+	public static void main(String[] args) {
+		int n = 4;
 //	for(int i=1;i<=n;i++) {
 //		for(int j=1;j<2*n;j++) {
 //			if(i+j>=n+1 && j-i<=n-1) {
@@ -14,10 +14,9 @@ public static void main(String[] args) {
 //		System.out.println();
 //		
 //	}
-	
-	
+
 //2
-	
+
 //	for(int i=1;i<=n;i++) {
 //		for(int j=1;j<2*n;j++) {
 //			if(i<=j && i+j<=2*n) {
@@ -29,9 +28,9 @@ public static void main(String[] args) {
 //		System.out.println();
 //		
 //	}
-	
+
 //3
-	
+
 //	for(int i=1;i<2*n;i++) {
 //		for(int j=1;j<=n;j++) {
 //			if(i>=j && i+j<=2*n) {
@@ -43,10 +42,9 @@ public static void main(String[] args) {
 //		System.out.println();
 //		
 //	}
-	
 
 // 4
-	
+
 //	for(int i=1;i<2*n;i++) {
 //		for(int j=1;j<=n;j++) {
 //			if(i+j>=n+1 && i-j<=n-1) {
@@ -58,9 +56,7 @@ public static void main(String[] args) {
 //		System.out.println();
 //		
 //	}
-	
-	
-	
+
 //5
 //	for(int i=1;i<2*n;i++) {
 //		for(int j=1;j<2*n;j++) {
@@ -73,9 +69,9 @@ public static void main(String[] args) {
 //		System.out.println();
 //	}
 //	
-	
+
 //6
-	
+
 //	for(int i=1;i<=n;i++) {
 //		for(int j=1;j<2*n;j++) {
 //			if(i==4 || i+j==n+1 || j-i==n-1) {
@@ -86,13 +82,11 @@ public static void main(String[] args) {
 //		}
 //		System.out.println();
 //	}
-	
-	
+
 //Double triangles
-	
+
 //1
-	
-	
+
 //	for(int i=1;i<=n;i++) {
 //		for(int j=1;j<2*n;j++) {
 //			if(i>=j || j==2*n-1 || i==4 || i+j>=2*n  ) {
@@ -103,8 +97,7 @@ public static void main(String[] args) {
 //		}
 //		System.out.println();
 //	}
-	
-	
+
 //2
 //	for(int i=1;i<=n;i++) {
 //		for(int j=1;j<2*n;j++) {
@@ -116,36 +109,21 @@ public static void main(String[] args) {
 //		}
 //		System.out.println();
 //	}
-	
-	
 
-	
 //butter fly
-	
-	for(int i=1;i<2*n;i++) {
-		for(int j=1;j<2*n;j++) {
-			if(i+j==2*n || i==j || i==n || j==n-3 || j==2*n-1) {
-				System.out.print("* ");
-			}else {
-				System.out.print("  ");
+
+		for (int i = 1; i < 2 * n; i++) {
+			for (int j = 1; j < 2 * n; j++) {
+				if (i + j == 2 * n || i == j || i == n || j == n - 3 || j == 2 * n - 1) {
+					System.out.print("* ");
+				} else {
+					System.out.print("  ");
+				}
 			}
+			System.out.println();
 		}
-		System.out.println();
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	//square with hallow
+
+		// square with hallow
 //	for(int i=1;i<2*n;i++) {
 //		for(int j=1;j<2*n;j++) {
 //			if(!(i+j>=n+1 && i-j<=n-1 && j-i<=n-1 && i+j<=3*n-1)) {
@@ -156,5 +134,5 @@ public static void main(String[] args) {
 //		}
 //		System.out.println();
 //	}
-}
+	}
 }
--- src/Methods/MethodsIntroduction.java
+++ src/Methods/MethodsIntroduction.java
@@ -3,8 +3,42 @@ package Methods;
 public class MethodsIntroduction {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	System.out.println(add(140,3));
+//		circleArea();
+//		TriangleArea();
+//		SquareArea();
 
 	}
+
+	public static void print() {
+		System.out.println("Hello from print function");
+	}
+	
+// Non-Parameterized Functions
+
+//	static void circleArea() {
+//		int r = 5;
+//		int ans = (int) 3.14 * r * r;
+//		System.out.println("Area of the circle is : " + ans);
+//	}
+//
+//	static void TriangleArea() {
+//		int b = 5;
+//		int h = 23;
+//		float ans = 0.5f * b * h;
+//		System.out.println("Area of the Triangle is : " + ans);
+//	}
+//
+//	static void SquareArea() {
+//		int s = 5;
+//		int ans = s * s;
+//		System.out.println("Area of the Square is : " + ans);
+//	}
+	
+// Argument Based Functions
+	
+public static int add(int a,int b) {
+	return a+b;
+}
 
 }
